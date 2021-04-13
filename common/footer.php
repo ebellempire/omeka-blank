3ff2fbafbac824e12050ba0ae87af8cb36c9fa98
Erin Bell <ebellempire@gmail.com>
2021-04-12 21:08:07 -0400

    Initial commit

---
+++ common/footer.php
@@ -0,0 +1,45 @@
+    </article>
+
+    <footer role="contentinfo">
+
+        <?php if (get_theme_option('footer_nav') == 1):?>
+        <!-- Footer Nav -->
+        <div id="nav-container" class="bottom">
+            <nav class="center" id="bottom-nav" role="navigation">
+                <?php echo public_nav_main(); ?>
+            </nav>
+            <div class="search-container center" role="search">
+                <?php echo search_form(); ?>
+            </div>
+        </div>
+        <?php endif;?>
+
+        <div id="footer-text">
+            <?php // Address
+            $site_orgname = trim(get_theme_option("site_orgname"));
+            $site_address = trim(get_theme_option("site_address"));
+            $site_phone = trim(get_theme_option("site_phone"));
+            $site_email = trim(get_theme_option("site_email"));
+            echo ($site_orgname || $site_address) ? "<p class='site-info address'>".implode(' | ', array($site_orgname,$site_address))."</p>" : null;
+            echo ($site_phone || $site_email) ? "<p class='site-info contact'>".implode(' | ', array($site_phone,$site_email))."</p>" : null;
+            ?>
+
+            <?php // Copyright
+            if ((get_theme_option('footer_copyright') == 1) && $copyright = option('copyright')) {
+                echo '<p class="site-info copyright">&copy; '.$copyright.'</p>';
+            } ?>
+
+            <?php // Powered By
+            if ((get_theme_option('omeka') == 1)) {
+                echo '<p class="site-info cms">'.__('Proudly powered by <a href="http://omeka.org">Omeka</a>.').'</p>';
+            }?>
+        </div>
+
+        <?php fire_plugin_hook('public_footer', array('view' => $this)); ?>
+
+    </footer>
+
+    </div><!-- end wrap -->
+    </body>
+
+    </html>
---
+++ common/header.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="<?php echo get_html_lang(); ?>">
+
+<head>
+    <meta charset="utf-8">
+    <?php if ($author = option('author')): ?>
+    <meta name="author" content="<?php echo $author; ?>" />
+    <?php endif; ?>
+    <?php if ($copyright = option('copyright')): ?>
+    <meta name="copyright" content="<?php echo $copyright; ?>" />
+    <?php endif; ?>
+    <?php if ($description = option('description')): ?>
+    <meta name="description" content="<?php echo $description; ?>" />
+    <?php endif; ?>
+    <?php
+    if (isset($title)) {
+        $titleParts[] = strip_formatting($title);
+    }
+    $titleParts[] = option('site_title');
+    if (!isset($title) && get_theme_option('site_subheading')) {
+        $titleParts[] = get_theme_option('site_subheading');
+    }
+    ?>
+    <title><?php echo implode(' &middot; ', $titleParts); ?></title>
+
+    <?php echo auto_discovery_link_tags(); ?>
+
+    <!-- Plugin Stuff -->
+    <?php fire_plugin_hook('public_head', array('view' => $this)); ?>
+
+    <!-- Stylesheets -->
+    <?php
+    queue_css_file('normalize');
+    queue_css_file('style');
+    if ($typekit_id = trim(get_theme_option('typekit'))) {
+        queue_css_url('https://use.typekit.net/'.$typekit_id.'.css');
+    }
+    echo head_css();
+    ?>
+
+    <!-- JavaScripts -->
+    <?php echo head_js(); ?>
+</head>
+
+<?php echo body_tag(array('id' => @$bodyid, 'class' => @$bodyclass)); ?>
+<a href="#content" id="skipnav"><?php echo __('Skip to main content'); ?></a>
+<?php fire_plugin_hook('public_body', array('view' => $this)); ?>
+<div id="wrap">
+
+    <header role="banner">
+
+        <?php fire_plugin_hook('public_header', array('view' => $this)); ?>
+
+        <div id="banner-container">
+            <div id="site-title">
+                <h1><?php echo link_to_home_page(theme_logo()); ?></h1>
+                <?php if ($sh = get_theme_option('site_subheading')):?>
+                <?php echo '<h2>'.$sh.'</h2>';?>
+                <?php endif; ?>
+            </div>
+        </div>
+
+        <!-- Header Nav -->
+        <div id="nav-container" class="top">
+            <nav class="center" id="top-nav" role="navigation">
+                <?php echo public_nav_main(); ?>
+            </nav>
+        </div>
+        <div class="search-container center" role="search">
+            <?php echo search_form(); ?>
+        </div>
+
+    </header>
+
+    <article id="content" role="main">
+
+        <?php fire_plugin_hook('public_content_top', array('view' => $this)); ?>
